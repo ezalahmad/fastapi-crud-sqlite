603ad4116f5f7612c59db87061b61f8bd6b2fafe
El pythonhat <ask.pythonhat@gmail.com>
2024-11-10 23:58:26 +0800

    Create or insert data `todo` app

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
  # main.py
 
 from typing import Union
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends, Form, status
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from fastapi.responses import RedirectResponse
+
 
 import models
 from database import SessionLocal, engine
@@ -23,7 +25,15 @@ def get_db():
         db.close()
 
 @app.get("/")
-def index(request: Request):
+def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post("/add")
+def add(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
+    new_todo = models.Todo(title=title)
+    db.add(new_todo)
+    db.commit()
+
+    url = app.url_path_for("home")
+    return RedirectResponse(url=url, status_code=status.HTTP_303_SEE_OTHER)
 
