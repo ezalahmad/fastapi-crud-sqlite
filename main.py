4ae97cce641d0c6b0d8d05bf21cb1e525c761761
El pythonhat <ask.pythonhat@gmail.com>
2024-11-10 22:27:53 +0800

    Initiate sqlite database

--- main.py
+++ main.py
@@ -4,10 +4,23 @@
 from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def index(request: Request):
